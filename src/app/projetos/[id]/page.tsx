19022bd42999a47be089151ad2d26a6c4acf9316
Laís Sampaio <laissampaio308@gmail.com>
2025-07-03 04:58:22 -0300

    Corrigindo

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -2,9 +2,7 @@ import { projetos } from '@/data/projetos'
 import { notFound } from 'next/navigation'
 import ProjetoDetalhesClient from './ProjetoDetalhesClient'
 
-interface Props {
-  params: { id: string }
-}
+
 
 export async function generateStaticParams() {
   return projetos.map((projeto) => ({
@@ -12,7 +10,8 @@ export async function generateStaticParams() {
   }))
 }
 
-export default async function ProjetoPage({ params }: Props) {
+export default function ProjetoPage({ params }: { params: { id: string } }) {
+  
   const projeto = projetos.find((p) => p.id === params.id)
 
   if (!projeto) return notFound()
